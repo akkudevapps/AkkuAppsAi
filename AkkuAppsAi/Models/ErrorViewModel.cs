8b5aa1648474e8fbc4147f80349c2676f4f78a18
dharini767-dev <leoinfotech.chinnamanur@gmail.com>
2026-05-19 02:31:10 +0530

    Add project files.

---
+++ AkkuAppsAi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AkkuAppsAi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
